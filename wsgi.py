f59d7469d009b3d6ecb0531b250d7e2aad81a058
Roman <rbkmob@gmail.com>
2019-03-20 01:49:17 +0700

    изменил wsgi добавил ссл сертификат2

--- wsgi.py
+++ wsgi.py
@@ -6,9 +6,10 @@ from OpenSSL import SSL
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file(dir_path + '/key.pem')
-context.use_certificate_file(dir_path + '/cert.pem')
+
+key = dir_path + '/key.pem'
+cert = dir_path + '/cert.pem'
+context = (cert, key)
 
 if __name__ == "__main__":
     app.run(ssl_context=context)
